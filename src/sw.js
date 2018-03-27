8ed0ec8f699f6f9fb60e1fc08ac10fb674e18e59
Sam-Guliker <samguliker@hotmail.com>
2018-03-27 11:19:31 +0200

    ?

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,15 @@
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
-
-self.addEventListener('fetch', event => {
-    //event.respondWith(new Response('hijacked directly!'));
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('bs-v1-core').then(function(cache) {
+      return cache.addAll(
+        [
+          '/css/bootstrap.css',
+          '/css/main.css',
+          '/js/bootstrap.min.js',
+          '/js/jquery.min.js',
+          '/offline'
+        ]
+      );
+    })
+  );
 });
