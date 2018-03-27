9991d5ce9d06604923fb17a14625c421120b8226
Sam-Guliker <samguliker@hotmail.com>
2018-03-27 10:56:28 +0200

    update

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('fetch', event => {
     event.respondWith(new Response('hijacked'));
 });
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting('BOOM'));
+});
