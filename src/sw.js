c3a86d29c666b6967f1f365edf5b2435ba079172
Sam-Guliker <samguliker@hotmail.com>
2018-03-27 14:51:05 +0200

    ?

--- src/sw.js
+++ src/sw.js
@@ -4,13 +4,10 @@ self.addEventListener('install', event => event.waitUntil(
         .then(self.skipWaiting())
 ));
 
-/* check in DevTools > Application > Cache Storage
- * or check in console using:
-caches.open('bs-v1-core')
-.then(cache => cache.match('/offline/'))
-.then(res => res.text())
-.then(text => console.log(text))
-*/
+// caches.open('bs-v1-core')
+// .then(cache => cache.match('/offline/'))
+// .then(res => res.text())
+// .then(text => console.log(text))
 
 self.addEventListener('fetch', event => {
     event.respondWith(
